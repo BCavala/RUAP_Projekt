1b5995bf94367684caea9006a973992f2c8bbcf9
BCavala <cabruno@gmail.com>
2023-11-07 09:22:16 +0100

    komentar

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Pozdrav iz aplikacije od Bruno Cavala");
+Console.WriteLine("Pozdrav iz aplikacije od Bruno Cavala, moj broj 4+6568+8");
